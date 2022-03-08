34ca30b00ed6ec9ebf83be418bb91d4a47a50b9d
vatteGopi <gopivatte21@gmail.com>
2022-03-08 22:28:22 +0530

    phase4

---
+++ phase4/assisted project/Assisted_Projects/AngularApps/FirstAngularApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GreetComponent } from './greet/greet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GreetComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
